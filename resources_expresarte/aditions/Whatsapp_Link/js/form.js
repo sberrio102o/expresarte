c70094dcdbda7d6657629e480a181325b99711d2
samu102o <sberrio0003@gmail.com>
2026-08-20 23:25:51 -0500

    More functionalities added

---
+++ resources_expresarte/aditions/Whatsapp_Link/js/form.js
@@ -0,0 +1,39 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  const telefono = "573116876773";
+
+  const cliente = document.querySelector("#cliente").value;
+  const fecha = document.querySelector("#fecha").value;
+  const hora = document.querySelector("#hora").value;
+  
+  const servicio = document.querySelector("#servicio").value;
+  const resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_MI NEGOCIO_*%0A
+		*Reservas*%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A
+		*Indica la fecha de tu reserva*%0A
+		${fecha}%0A
+		*Indica la hora de tu reserva*%0A
+		${hora}%0A
+		*¿Cuál es el servicio que se desea realizar?*%0A
+		${servicio}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+
+  window.open(url);
+});
